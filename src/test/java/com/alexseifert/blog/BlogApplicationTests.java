83e7d34ad205c6d3d660bd57c8eb6a3eb4d40499
Alex Seifert <alex@alexseifert.com>
2023-01-13 17:12:07 +0100

    Initial commit

---
+++ src/test/java/com/alexseifert/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexseifert.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
