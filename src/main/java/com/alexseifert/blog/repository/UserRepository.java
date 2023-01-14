e58de96cf774b28a48c2cc54ab5ef22cef5acea0
Alex Seifert <alex@alexseifert.com>
2023-01-14 20:34:06 +0100

    Add the respositories

---
+++ src/main/java/com/alexseifert/blog/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.alexseifert.blog.repository;
+
+import com.alexseifert.blog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  User findByEmail(String email);
+}
