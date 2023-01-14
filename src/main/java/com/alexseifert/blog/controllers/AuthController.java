4d32b1b1c165b3d0453ee3dd321b3a15af470311
Alex Seifert <alex@alexseifert.com>
2023-01-14 23:00:10 +0100

    Add the register post endpoint

--- src/main/java/com/alexseifert/blog/controllers/AuthController.java
+++ src/main/java/com/alexseifert/blog/controllers/AuthController.java
@@ -1,10 +1,25 @@
 package com.alexseifert.blog.controllers;
 
+import com.alexseifert.blog.dto.UserDto;
+import com.alexseifert.blog.entity.User;
+import com.alexseifert.blog.service.UserService;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import jakarta.validation.Valid;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 
 @Controller
 public class AuthController {
+
+  private UserService userService;
+
+  public AuthController(UserService userService) {
+    this.userService = userService;
+  }
 
   @GetMapping("/login")
   public String login() {
@@ -14,6 +29,26 @@ public class AuthController {
   @GetMapping("/register")
   public String register() {
     return "register";
+  }
+
+  @PostMapping("/register")
+  public String registerSave(@Valid @ModelAttribute("user") UserDto user,
+                             BindingResult result,
+                             Model model) {
+    User existing = userService.findByEmail(user.getEmail());
+
+    if (existing != null) {
+        result.rejectValue("email", null, "There is already an account registered with that email");
+    }
+
+    if (result.hasErrors()) {
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    userService.saveUser(user);
+
+    return "redirect:/register?success";
   }
 
 }
